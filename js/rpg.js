ce7e2d898b3c7c41669e31bf4debc9c99953ec50
Rose Koenings <rnkings.05@gmail.com>
2016-10-06 15:03:53 -0500

    added ability to change health and gold to character type

--- js/rpg.js
+++ js/rpg.js
@@ -1,11 +1,39 @@
 "use strict";
 
 
-]function Character(type) {
+function Character(type) {
     this.health = 100;
     this.gold = 1;
     this.type = type;
+
+    //making random events based on how much gold or health they give or make you lose
+	var badEvents = [{situation: "clawed by bear", healthAmount: 2, goldAmount: 5}];
+
+	this.badEventsConsequences = function(consequences){
+		if(consequences.hasOwnProperty("situation") && consequences.hasOwnProperty("healthAmount" && consequences.hasOwnProperty("goldAmount")))
+			badEvents.push(consequences);
+	}
+
 }
+this.badEventsTotal = function(){
+	var total = 0;
+	badEvents.forEach(function(badEvent)
+		if(badEvent.situation === "clawed by bear"){
+			total += badEvent.healthAmount && total += badEvent.goldAmount;
+		}
+		else{
+			total -= badEvent.healthAmount && total += badEvent.goldAmount;
+		}
+	});
+	return total;
+};
+//getFull?
+Character.prototype.getFull = function(){
+	return this.health + " " + this.gold + " " this.type;
+};
+Character.prototype.getProfile = function(){
+	return this.getFull() + ", bad events total: " + this.badEventsTotal();
+};
 
 //This is attaching methods to our class
 Character.prototype.getHealth = function () {
@@ -42,6 +70,8 @@ function WarriorMonk() {
 WarriorMonk.prototype = Object.create(Character.prototype);
 //We should set the constructor on here to our constructor
 WarriorMonk.prototype.constructor = WarriorMonk;
+
+
 
 function chooseCharacter(type) {
     var player;
