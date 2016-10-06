13892cbf8916aba29010400b099c4d7e9a94c24a
Michael <mrytz55@gmail.com>
2016-10-06 10:53:56 -0500

    Added on to the prototype for our characters, created variables which are new prototypes of the character.

--- js/rpg.js
+++ js/rpg.js
@@ -1,12 +1,24 @@
 "use strict";
 
 
-function character(){};
+function character(health, gold, name){};
 
 character.prototype.health = 100;
 
+character.prototype.gold = 0;
+
+character.prototype.name = name;
+
 character.prototype.getHealth = function(){
 	return this.health;
+}
+
+character.prototype.getGold = function(){
+	return this.gold;
+}
+
+character.prototype.getName = function(){
+	return this.name;
 }
 
 //inheritance 
@@ -15,4 +27,9 @@ function questor(){
 }
 
 
-battleNun.prototype = new character;
+var battleNun = new character(100, 0, "Battle Nun");
+
+var assassinPriest = new character(100, 0, "Assassin Priest");
+
+var warriorMonk = new character(100, 0, "Warrior Monk");
+
