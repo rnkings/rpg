c26163ff3377f8c9bb184e7b28f1046521e54c7d
Rose Koenings <rnkings.05@gmail.com>
2016-10-06 10:22:24 -0500

    added character class and battleNun prototype inheritance

--- js/rpg.js
+++ js/rpg.js
@@ -1,1 +1,14 @@
 "use strict";
+
+
+function character(){};
+
+character.prototype.health = 200;
+
+character.prototype.getHealth = function(){
+	return this.health;
+}
+
+//inheritance 
+
+battleNun.prototype = new character;
